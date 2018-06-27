af72361dc374989ec39e2dc3c41df9565149a6c1
ababyduck <kassisaf@gmail.com>
2018-06-27 15:46:40 -0700

    Added regex patterns for guessing expiration dates

--- src/main/java/Scraper.java
+++ src/main/java/Scraper.java
@@ -23,7 +23,11 @@ public class Scraper {
     private static ResourceBundle credentialsBundle = ResourceBundle.getBundle("credentials");
 
     public static void main(String[] args){
+        ScrapeSubreddit("GameDeals");
+//        ScrapeSubreddit("AndroidGameDeals");
+    }
 
+    public static void ScrapeSubreddit(String targetSub){
         // Set up our reddit connection and auth
         Credentials credentialsReddit = Credentials.script(
                 credentialsBundle.getString("reddit_username"),
@@ -34,9 +38,8 @@ public class Scraper {
         NetworkAdapter adapter = new OkHttpNetworkAdapter(userAgent);
         RedditClient reddit = OAuthHelper.automatic(adapter, credentialsReddit);
 
-        // Get submissions from the subs we care about
-        // TODO: iterate over a list of relevant subs to get around 100 submission limit
-        DefaultPaginator<Submission> dealSubs = reddit.subreddits("GameDeals", "AndroidGameDeals").posts()
+        // Get 100 most recent submissions from the target subreddit
+        DefaultPaginator<Submission> dealSubs = reddit.subreddit(targetSub).posts()
                 .sorting(SubredditSort.NEW)
                 .timePeriod(TimePeriod.MONTH)
                 .limit(100)
@@ -52,21 +55,24 @@ public class Scraper {
             String title = s.getTitle();
             // This regex pattern looks for "free", but tries to avoid hitting games with "free" in the title,
             // e.g. "Freedom Fighters", or non-free games containing a "free gift".
-            String pattern = ".*(?i)[\\W](free)(?!( gift))[\\W].*";
-            if (title.toLowerCase().matches(pattern)){
+            String reFreeGame = ".*(?i)[\\W](free)(?!( gift))[\\W].*";
+            if (title.toLowerCase().matches(reFreeGame)){
                 freeCount++;
 
-                // Parse submission title into data we care about and store it in a new Deal object
-                String delims = "[\\[\\]()]+";
-                String[] tokens = title.split(delims);
+                // Parse submission title into data we care about
+                String reTitleDelimiters = "[\\[\\]()]+";
+                String[] tokens = title.split(reTitleDelimiters);
+                // TODO: Add exception handling for submissions that don't split cleanly into 3+ elements
                 String dealPlatform = tokens[1];
                 String dealTitle = tokens[2].trim();
 
+                // Create a new deal object and populate it with relevant data
                 Deal deal = new Deal(dealTitle, s.getUrl());
                 deal.setPlatform(dealPlatform);
                 deal.setSource("Reddit user " + s.getAuthor());
                 deal.setSourceURL("https://www.reddit.com" + s.getPermalink());
                 deal.setPostDate(s.getCreated());
+                // Try to determine whether title is a full game or just a DLC
                 if (title.contains("DLC")) {
                     deal.setType(DealTypes.DLC);
                 }
@@ -74,13 +80,18 @@ public class Scraper {
                     deal.setType(DealTypes.FULL_GAME);
                 }
                 // TODO: Try to determine expiration date based on submission time + information in title, e.g. "Free for 48 hrs"
+                String reFreeForDuration = ".*(?i)(free)[\\W]((for)[\\W][\\d*].*((hr)|(hour)|(day)|(week))).*";
+                String reFreeUntilDate = ".*(?i)(free)[\\W](until)[\\W].*[\\d*].*";
+                if (title.matches("")) {
+
+                }
 
                 System.out.println(deal.toString());
             }
         }
         System.out.println("---");
-        System.out.print("Found " + freeCount + " free titles out of " + viewCount + " submissions viewed.");
-
+        System.out.print("/r/" + targetSub + ": " +
+                "Found " + freeCount + " free titles out of " + viewCount + " submissions viewed.");
     }
 
 }
