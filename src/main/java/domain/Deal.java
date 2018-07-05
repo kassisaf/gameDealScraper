0592aa1c1b90957a061976b58c3f4788f1d10300
ababyduck <kassisaf@gmail.com>
2018-07-05 03:02:12 -0700

    Rewrote Deal as an abstract superclass
    - RedditDeal is compliant, still working on HumbleDeal
    - Modified RedditDeal.isFree() regex to filter out "free shipping"

--- src/main/java/domain/Deal.java
+++ src/main/java/domain/Deal.java
@@ -1,171 +1,62 @@
 package domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import scraper.Convert;
+import org.apache.logging.log4j.util.Strings;
 
 import java.math.BigDecimal;
 import java.net.URL;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
-public class Deal {
-    private String       title;
-    private URL          url;
-    private String       store;
-    private List<String> platforms;
-    private LocalDate    datePosted;
-    private LocalDate    dateExpires;
-    private BigDecimal   normalPrice;
-    private BigDecimal   currentPrice;
-    private String       sourceName;
-    private URL          sourceUrl;
-    private URL          imageUrl;
+public abstract class Deal {
+    // All deals should be able to implement these fields as a minimum
+    String       title;
+    URL          url;
+    String       store;
+    String       sourceName;
+    URL          sourceUrl;
+    // These fields may not be available from all sources and are therefore optional
+    LocalDate    expiry;
+    List<String> platforms;
+    BigDecimal   normalPrice;
+    BigDecimal   currentPrice;
+    URL          imageUrl;
 
-    // Constructors
-    public Deal() {
+    Deal() {
     }
-    public Deal(String title, URL url) {
-        this.setTitle(title);
-        this.setUrl(url);
-    }
-    public Deal(String title, String url) {
-        this.setTitle(title);
-        this.setUrl(url);
-    }
-    // TODO: Write constructors that will accept classes returned by scraper.WebScraper
 
-
-
-    // Public Methods
     @Override
     public String toString() {
-        // TODO: Expand output. StringBuilder or StringBuffer may be preferable here.
-        // TODO: Rewrite using StringBuilder or StringBuffer. This is ugly and bad.
-        String prefix = "";
-        if (store != "" && store != null) {
-            prefix = "[" + store + "] ";
-        }
-        String secondLine = "\n" + url.toString();
+        StringBuilder s = new StringBuilder();
 
-        return (prefix + title + secondLine);
+        if (!Strings.isEmpty(store)) {
+            s.append("[").append(store).append("] ");
+        }
+        s.append(title);
+        s.append("\n").append(url.toString());
+
+        return s.toString();
     }
 
-    public String toJsonString() throws JsonProcessingException {
+    public String toJson() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.writeValueAsString(this);
     }
 
-    @JsonIgnore
     public Boolean isFree() {
-        // TODO: Exception for missing data
-        return currentPrice.intValue() == 0;
+        return currentPrice.equals(BigDecimal.valueOf(0));
     }
 
-    @JsonIgnore
     public Boolean isExpired() {
-        // TODO: Exception for missing data
-        return dateExpires.isBefore(LocalDate.now());
+        return expiry.isBefore(LocalDate.now());
     }
 
-
-
-    // Getters & Setters
     public String getTitle() {
         return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public URL getUrl() {
         return url;
-    }
-    public void setUrl(URL url) {
-        this.url = url;
-    }
-    public void setUrl(String url) {
-        this.url = Convert.stringToURL(url);
-    }
-
-    public LocalDate getDatePosted() {
-        return datePosted;
-    }
-    public void setDatePosted(LocalDate datePosted) {
-        this.datePosted = datePosted;
-    }
-    // TODO: Add setters for Date variables that accept and parse a String representation (epoch/NLP)
-
-    public LocalDate getDateExpires() {
-        return dateExpires;
-    }
-    public void setDateExpires(LocalDate dateExpires) {
-        this.dateExpires = dateExpires;
-    }
-
-    public List<String> getPlatforms() {
-        return platforms;
-    }
-    public void setPlatforms(List<String> platforms) {
-        this.platforms = platforms;
-    }
-    public void setPlatforms(String platforms) {
-        List<String> l = new ArrayList<>();
-        l.add(platforms);
-        this.platforms = l;
-    }
-
-    public String getStore() {
-        return store;
-    }
-    public void setStore(String store) {
-        this.store = store;
-    }
-    public void setStore(List<String> storeList) {
-        this.store = String.join("/", storeList);
-    }
-
-
-    public BigDecimal getNormalPrice() {
-        return normalPrice;
-    }
-    public void setNormalPrice(BigDecimal normalPrice) {
-        this.normalPrice = normalPrice;
-    }
-
-    public BigDecimal getCurrentPrice() {
-        return currentPrice;
-    }
-    public void setCurrentPrice(BigDecimal currentPrice) {
-        this.currentPrice = currentPrice;
-    }
-
-    public String getSourceName() {
-        return sourceName;
-    }
-    public void setSourceName(String sourceName) {
-        this.sourceName = sourceName;
-    }
-
-    public URL getSourceUrl() {
-        return sourceUrl;
-    }
-    public void setSourceUrl(URL sourceUrl) {
-        this.sourceUrl = sourceUrl;
-    }
-    public void setSourceUrl(String sourceUrl) {
-        this.sourceUrl = Convert.stringToURL(sourceUrl);
-    }
-
-    public URL getImageUrl() {
-        return imageUrl;
-    }
-    public void setImageUrl(URL imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = Convert.stringToURL(imageUrl);
     }
 }
--- src/main/java/domain/RedditDeal.java
+++ src/main/java/domain/RedditDeal.java
@@ -3,57 +3,60 @@ package domain;
 import net.dean.jraw.models.Submission;
 import scraper.Convert;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class RedditDeal {
-    private Submission submission;
-    private String rawTitle;
-    private String parsedTitle;
-    private String parsedStore;
+public class RedditDeal extends Deal {
+    private final Submission submission;
+    private final String rawTitle;
 
     public RedditDeal(Submission submission) {
+        super();
         this.submission = submission;
         rawTitle = submission.getTitle();
+        parseSubmissionTitle(); // Updates title field, and store field if submission title can be parsed cleanly
+        url = Convert.stringToURL(submission.getUrl());
+        sourceName = "Reddit (/u/" + submission.getAuthor() + " via /r/" + submission.getSubreddit() + ")";
+        sourceUrl = Convert.stringToURL("https://www.reddit.com" + submission.getPermalink());
+        if (isFree()) {
+            currentPrice = BigDecimal.valueOf(0);
+        }
+    }
+
+    @Override
+    public Boolean isFree() {
+        // This regex pattern looks for "free", but tries to avoid hitting games with "free" in the title,
+        // e.g. "Freedom Fighters", or non-free games indicating "free gift," "free weekend," or "free shipping"
+        String reFreeGame = ".*(?i)[\\W](free)(?!( gift)|( weekend)|( shipping))[\\W].*";
+        // Filter out "buy 2 get 1" (Gamestop spam)
+        String reBuyOneGetOne = ".*(?i)(buy) [\\d] (get).*";
+        return (rawTitle.matches(reFreeGame) && !rawTitle.matches(reBuyOneGetOne));
+    }
+
+    private void parseSubmissionTitle()
+    {
         // Attempt to parse the submission title into useful info
         // Note: This split relies on user submissions following /r/GameDeals rule #3 title formatting:
         // https://www.reddit.com/r/GameDeals/wiki/rules#wiki_3._title_format
         // TODO: Handle subreddit-specific delimiters
+
         String reTitleDelimiters = "[\\[\\]()]+";
         List<String> splitTitle = new ArrayList<>(Arrays.asList(rawTitle.split(reTitleDelimiters)));
         splitTitle.removeAll(Arrays.asList("", null));
         // If the submission title doesn't split cleanly into our expected number of elements, disregard split results.
         if (splitTitle.size() >= 3) {
-            parsedStore = splitTitle.get(0).trim();
-            parsedTitle = splitTitle.get(1).trim();
+            store = splitTitle.get(0).trim();
+            title = splitTitle.get(1).trim();
         }
         else {
-            parsedTitle = rawTitle;
+            title = rawTitle;
         }
+
         // TODO: Attempt to parse expiration date from submission title
         // These regex patterns may be useful:
         //String reFreeForDuration = ".*(?i)(free)[\\W]((for)[\\W][\\d*].*((hr)|(hour)|(day)|(week))).*";
         //String reFreeUntilDate = ".*(?i)(free)[\\W](until)[\\W].*[\\d*].*";
-    }
-
-    public Deal toDeal() {
-        Deal deal = new Deal(parsedTitle, submission.getUrl());
-        if (parsedStore != null) {
-            deal.setStore(parsedStore);
-        }
-        deal.setSourceName("Reddit (/u/" + submission.getAuthor() + " via /r/" + submission.getSubreddit() + ")");
-        deal.setSourceUrl("https://www.reddit.com" + submission.getPermalink());
-        deal.setDatePosted(Convert.dateToLocalDate(submission.getCreated()));
-        return deal;
-    }
-
-    public Boolean isFree() {
-        // This regex pattern looks for "free", but tries to avoid hitting games with "free" in the rawTitle,
-        // e.g. "Freedom Fighters", or non-free games containing a "free gift" or a "free weekend".
-        String reFreeGame = ".*(?i)[\\W](free)(?!( gift)|( weekend))[\\W].*";
-        // Filter out "buy 2 get 1" (Gamestop spam)
-        String reBuyOneGetOne = ".*(?i)(buy) [\\d] (get).*";
-        return (rawTitle.matches(reFreeGame) && !rawTitle.matches(reBuyOneGetOne));
     }
 }
