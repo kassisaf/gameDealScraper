4e0be4463158a49ea7783a4a43611def2d39abce
ababyduck <kassisaf@gmail.com>
2018-07-08 05:23:04 -0700

    Hardwired humble store scrape to "/" request for now, layout works*!
    - Icons remain static for now. Currently these are stored as strings, so
    we need a way to translate them to icons for Thymeleaf.

--- src/main/java/com/akassis/gamedealscraper/webapp/DealsController.java
+++ src/main/java/com/akassis/gamedealscraper/webapp/DealsController.java
@@ -1,24 +1,37 @@
 package com.akassis.gamedealscraper.webapp;
 
+import com.akassis.gamedealscraper.domain.Deal;
+import com.akassis.gamedealscraper.scraper.Scraper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.util.List;
+
+import static com.akassis.gamedealscraper.scraper.Scraper.scrapeHumbleStore;
+
 @Controller
 public class DealsController {
 
     @RequestMapping(value = "/")
-    public String deals(HttpServletRequest request, Model model) {
-        String name = request.getParameter("name");
-
-        if (name == null) {
-            name = "world";
-        }
-        name = null;
-
-        model.addAttribute("name", name);
+    public String deals(Model model) {
+        List<Deal> deals = Scraper.scrapeHumbleStore();
+        model.addAttribute("deals", deals);
         return "base";
+
+
+//    @RequestMapping(value = "/")
+//    public String deals(HttpServletRequest request, Model model) {
+//        String name = request.getParameter("name");
+//
+//        if (name == null) {
+//            name = "world";
+//        }
+//        name = null;
+//
+//        model.addAttribute("name", name);
+//        return "base";
     }
 }
