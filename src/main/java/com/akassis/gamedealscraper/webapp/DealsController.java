0ca133a73f5f710a46c97207680166fbce966bb3
ababyduck <kassisaf@gmail.com>
2018-07-09 00:47:02 -0700

    Platform icons now display correctly (store/vendors still needs work)
    - Need to change the signature of `store` to List rather than String,
    and kill the ugly string.join in HumbleDealSerializer so that we can
    iterate over multiple values.
    - Consider renaming `store` to `vendors`

--- src/main/java/com/akassis/gamedealscraper/webapp/DealsController.java
+++ src/main/java/com/akassis/gamedealscraper/webapp/DealsController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -13,12 +14,19 @@ public class DealsController {
 
     @RequestMapping(value = "/")
     public String deals(Model model) {
-//        List<Deal> deals = Scraper.scrapeHumbleStore();
 
-        List<Deal> deals = Scraper.scrapeSubreddit("GameDeals");
-        deals = Scraper.getFreeResults(deals);
+        List<Deal> deals = new ArrayList<>();
+
+        List<Deal> humbleDeals = Scraper.scrapeHumbleStore();
+        deals.addAll(humbleDeals);
+
+//        List<Deal> redditDeals = Scraper.scrapeSubreddit("GameDeals");
+//        deals.addAll(redditDeals);
+
+//        deals = Scraper.getFreeResults(deals);
 
         model.addAttribute("deals", deals);
+        model.addAttribute("iconMap", Deal.iconMap);
         return "base";
     }
 }
